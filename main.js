22a4daa000de94d160f0d1f50738b82700f1c413
Yi Ge <me@yige.ch>
2018-06-25 18:55:24 +0200

    add initial files

---
+++ main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow} = require('electron');
+try {
+  require('electron-reloader')(module);
+} catch (err) {}
+
+
+let mainWindow = null
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 400, 
+    height: 800,
+    titleBarStyle: 'hidden',
+    backgroundColor: "#484848",
+  })
+
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
