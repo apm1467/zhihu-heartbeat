229153750aae8ab329fbc7458d98af8baada0bc2
Yi Ge <me@yige.ch>
2018-06-27 12:14:33 +0200

    open feed images in new window

--- renderer.js
+++ renderer.js
@@ -5,6 +5,34 @@ $(document).on('click', 'a[href^="http"]', function(event) {
     shell.openExternal(this.href);
 });
 
+// open feed images in new window
+const {BrowserWindow} = require('electron').remote;
+$(document).on('click', '.img', function(event) {
+    var img_url;
+    if ($(this).hasClass('single-img')) {
+        img_url = $(this).attr('src');
+    }
+    else {
+        img_url = $(this).css('background-image').slice(4, -1).replace(/"/g, "");
+    }
+    img_url = img_url.replace(/_[a-z]+/, "_qhd"); // get large image
+
+    var img = new Image();
+    img.src = img_url;
+    img.onload = function(){
+        var win = new BrowserWindow({
+            titleBarStyle: 'hidden',
+            show: false,
+            height: this.height,
+            width: this.width
+        });
+        win.loadURL(img_url);
+        win.once('ready-to-show', () => {
+            win.show();
+        });
+    };
+});
+
 
 var time = Date.now();
 var access_expire_time = localStorage.getItem('access_expire_time');
