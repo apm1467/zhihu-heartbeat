4c9fa22c6532253e8370951b770c0d712a38a9e1
Yi Ge <me@yige.ch>
2018-10-19 15:28:03 +0200

    use request-promise in auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,71 +1,66 @@
-const request = require('request');
+const crypto = require('crypto');
+const request = require('request-promise');
 const constants = require('./constants');
 
 const current_window = require('electron').remote.getCurrentWindow();
 
 
 exports.check_captcha = check_captcha;
-function check_captcha() {
+async function check_captcha() {
     var options = {
         method: 'GET',
         url: constants.CAPTCHA_URL,
         headers: constants.CAPTCHA_REQUEST_HEADER,
-        jar: true // accept cookie
+        jar: true,
+        json: true
     };
-    request(options, function(error, response, body) {
-        if (JSON.parse(body)['show_captcha']) {
-            get_captcha();
-        }
-    });
+    var res = await request(options);
+    if (res['show_captcha'])
+        get_captcha();
 }
 
-function get_captcha() {
+async function get_captcha() {
     var options = {
         method: 'PUT',
         url: constants.CAPTCHA_URL,
         headers: constants.LOGIN_REQUEST_HEADER,
-        jar: true
+        jar: true,
+        json: true
     };
-    request(options, function(error, response, body) {
-        if ('error' in JSON.parse(body)) {
-            check_captcha();
-            return;
-        }
+    var res = await request(options);
+    if ('error' in res) {
+        check_captcha();
+        return;
+    }
 
-        var image = new Image();
-        image.src = 'data:image/gif;base64,' + JSON.parse(body)['img_base64'];
-        $('.captcha').prepend(image);
-        $('.captcha').removeClass('hidden');
-    });
+    var image = new Image();
+    image.src = 'data:image/gif;base64,' + res['img_base64'];
+    $('.captcha').prepend(image);
+    $('.captcha').removeClass('hidden');
 }
 
 
-exports.get_access_token = function(email, password, captcha_text) {
+exports.get_access_token = async function(email, password, captcha_text) {
     if (captcha_text) {
         // submit captcha text before authentication
         var options = {
             method: 'POST',
             url: constants.CAPTCHA_URL,
             headers: constants.LOGIN_REQUEST_HEADER,
+            form: { input_text: captcha_text },
             jar: true,
-            form: { input_text: captcha_text }
+            json: true
         };
-        request(options, function(error, response, body) {
-            var body_dict = JSON.parse(body);
-            if (body_dict['error']) {
-                reload_login_page(body_dict['error']['message']);
-                return;
-            }
-
-            authenticate(email, password);
-        });
+        var res = await request(options);
+        if (res['error']) {
+            reload_login_page(res['error']['message']);
+            return;
+        }
     }
-    else {
-        authenticate(email, password);
-    }
+    authenticate(email, password);
 }
 
-function authenticate(email, password) {
+async function authenticate(email, password) {
     var auth_data = constants.AUTH_DATA;
     var time = Date.now();
     auth_data['timestamp'] = time;
@@ -77,33 +72,29 @@ function authenticate(email, password) {
         method: 'POST',
         url: constants.SIGN_IN_URL,
         form: auth_data,
-        jar: true
+        jar: true,
+        json: true
     };
-    request(options, function(error, response, body) {
-        var body_dict = JSON.parse(body);
-        if (body_dict['error']) {
-            reload_login_page(body_dict['error']['message']);
-            return;
-        }
+    var res = await request(options);
+    if (res['error']) {
+        reload_login_page(res['error']['message']);
+        return;
+    }
 
-        localStorage.setItem('access_token', body_dict['access_token']);
-        localStorage.setItem('self_user_id', body_dict['uid']);
+    localStorage.setItem('access_token', res['access_token']);
+    localStorage.setItem('self_user_id', res['uid']);
+    var access_expire = time + res['expires_in'] * 1000;
+    localStorage.setItem('access_expire_time', access_expire.toString());
 
-        var access_expire = time + body_dict['expires_in'] * 1000;
-        localStorage.setItem('access_expire_time', access_expire.toString());
+    $('.login-form').addClass('hidden');
+    $('.logo').removeClass('hidden');
 
-        $('.login-form').addClass('hidden');
-        $('.logo').removeClass('hidden');
-
-        // notify renderer
-        current_window.webContents.send('auth_finished');
-    });
+    // notify renderer
+    current_window.webContents.send('auth_finished');
 }
 
 function calculate_signature(auth_data) {
-    const crypto = require('crypto');
     var hmac = crypto.createHmac('sha1', constants.APP_SECRET);
-
     var msg = auth_data['grant_type'] + auth_data['client_id'] + auth_data['source'] 
         + auth_data['timestamp'];
     hmac.update(msg);
@@ -116,8 +107,7 @@ function reload_login_page(err_message) {
 }
 
 
-exports.get_authorized_request_header = get_authorized_request_header;
-function get_authorized_request_header() {
+exports.get_authorized_request_header = function() {
     // append access_token to base_request_header
     var access_token = localStorage.getItem('access_token');
     var header = constants.BASE_HEADER;
